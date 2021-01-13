81cfc42ae5722692fae93a725e836b35427c4dff
Anthony Villena <hiddenwaffle@users.noreply.github.com>
2021-01-13 11:38:46 -0600

    next

--- pipelines/ansible.groovy
+++ pipelines/ansible.groovy
@@ -1,5 +1,7 @@
 node {
+    git branch: 'main', url: 'https://github.com/hiddenwaffle/jinx.git'
     withCredentials([sshUserPrivateKey(credentialsId: 'hepburn-pk', keyFileVariable: 'HEPBURN_PK')]) {
       sh 'echo "HEPBURN_PK: ${HEPBURN_PK}"'
+      sh 'type ansible'
     }
 }
