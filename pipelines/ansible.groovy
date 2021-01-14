874d511337c5f56eac070a28b538995ded52eff9
Anthony Villena <hiddenwaffle@users.noreply.github.com>
2021-01-14 13:34:20 -0600

    next

--- pipelines/ansible.groovy
+++ pipelines/ansible.groovy
@@ -1,9 +1,10 @@
 node {
-    git branch: 'main', url: 'https://github.com/hiddenwaffle/jinx.git'
     def image = docker.build("tempdeleteme:47") // ${env.BUILD_ID}
     //docker.image('tempdeleteme:1').inside {
     image.inside {
         stage('Do it') {
+            sh 'ls -lrt'
+            sh 'pwd'
             withCredentials([sshUserPrivateKey(credentialsId: 'hepburn-pk', keyFileVariable: 'HEPBURN_PK')]) {
                 sh '''poetry run ansible -i inventory/hosts.yml roman -l hepburn.local -a 'free -h' --extra-vars "ansible_ssh_private_key_file=${HEPBURN_PK}" '''
             }
