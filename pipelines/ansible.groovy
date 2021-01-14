42689ea445ffd0c07d06ed73b7b4b77cbde86692
Anthony Villena <hiddenwaffle@users.noreply.github.com>
2021-01-14 13:51:09 -0600

    next

--- pipelines/ansible.groovy
+++ pipelines/ansible.groovy
@@ -8,6 +8,8 @@ node {
     //docker.image('tempdeleteme:1').inside {
     image.inside {
         stage('Do it') {
+            sh 'ls -lrt'
+            sh 'pwd'
             withCredentials([sshUserPrivateKey(credentialsId: 'hepburn-pk', keyFileVariable: 'HEPBURN_PK')]) {
                 sh '''poetry run ansible -i inventory/hosts.yml roman -l hepburn.local -a 'free -h' --extra-vars "ansible_ssh_private_key_file=${HEPBURN_PK}" '''
             }
