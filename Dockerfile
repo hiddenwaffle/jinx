1c769100f20d73ea8e4ec4a4f99a05ebf70fbd2c
Anthony Villena <hiddenwaffle@users.noreply.github.com>
2021-01-14 13:28:49 -0600

    next

---
+++ Dockerfile
@@ -0,0 +1,20 @@
+FROM python:3.9.1
+
+WORKDIR /work
+
+# Set up Poetry
+RUN curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python -
+ENV PATH "$PATH:/root/.poetry/bin"
+
+# Fake networking to allow Ansible to SSH to the Vagrant instances
+# WARNING: These values can/will probably change
+RUN \
+    echo "192.168.65.2 hepburn.local" >> /etc/hosts && \
+    echo "192.168.65.2 peck.local" >> /etc/hosts && \
+    mkdir ~/.ssh && chmod 700 ~/.ssh && \
+    echo "[hepburn.local]:2222 ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBO6WMqyabG5PbImHJTuuNaaVa2glHxrScWDBrFmROenvA4miDHPx5Q5ZqrSqjwJ4CtneDosKgqULVfm09S0hLGg=" >> ~/.ssh/known_hosts && \
+    echo "[peck.local]:2200 ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBKTORHuY2wx1VjenGYm8y6q3cyxz7IfFYDAPdZDnLJyV/9OJhMLZXd8jHg+XsrnNS25EoTg9mNWhDSFQCiFJdaA=" >> ~/.ssh/known_hosts
+
+# Set up dependencies
+COPY pyproject.toml poetry.lock ./
+RUN poetry install
--- pipelines/ansible.groovy
+++ pipelines/ansible.groovy
@@ -1,19 +1,11 @@
 node {
     git branch: 'main', url: 'https://github.com/hiddenwaffle/jinx.git'
-    docker.image('python:3.9.1').inside {
-        stage('Setup') {
-            sh 'curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python -'
-            sh '$HOME/.poetry/bin/poetry install'
-            sh '''mkdir ~/.ssh && chmod 700 ~/.ssh '''
-            sh '''echo "$(getent hosts host.docker.internal | awk '{ print $1 }') hepburn.local" >> /etc/hosts '''
-            sh '''echo "[hepburn.local]:2222 ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBO6WMqyabG5PbImHJTuuNaaVa2glHxrScWDBrFmROenvA4miDHPx5Q5ZqrSqjwJ4CtneDosKgqULVfm09S0hLGg=" >> ~/.ssh/known_hosts '''
-            sh '''echo "$(getent hosts host.docker.internal | awk '{ print $1 }') peck.local" >> /etc/hosts '''
-            sh '''echo "[peck.local]:2200 ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBKTORHuY2wx1VjenGYm8y6q3cyxz7IfFYDAPdZDnLJyV/9OJhMLZXd8jHg+XsrnNS25EoTg9mNWhDSFQCiFJdaA=" >> ~/.ssh/known_hosts '''
-        }
+    def inage = docker.build("tempdeleteme:${env.BUILD_ID}")
+    //docker.image('tempdeleteme:1').inside {
+    image.inside {
         stage('Do it') {
             withCredentials([sshUserPrivateKey(credentialsId: 'hepburn-pk', keyFileVariable: 'HEPBURN_PK')]) {
-                sh '''$HOME/.poetry/bin/poetry run ansible -i inventory/hosts.yml roman -l hepburn.local -a 'free -h' --extra-vars "ansible_ssh_private_key_file=${HEPBURN_PK}" '''
-                //sh '''ssh hepburn.local -p 2222 -i $HEPBURN_PK -v 'df -H' '''
+                sh '''poetry run ansible -i inventory/hosts.yml roman -l hepburn.local -a 'free -h' --extra-vars "ansible_ssh_private_key_file=${HEPBURN_PK}" '''
             }
         }
     }
