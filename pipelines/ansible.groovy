70d4b8988dd681cf304a6c46814918ac70f1e474
Anthony Villena <hiddenwaffle@users.noreply.github.com>
2021-01-17 11:26:28 -0600

    next

--- pipelines/ansible.groovy
+++ pipelines/ansible.groovy
@@ -4,9 +4,17 @@ node {
     }
     def image = docker.build("tempdeleteme:latest")
     image.inside('--add-host=hepburn.local:192.168.65.2 --add-host=peck.local:192.168.65.2') {
-        stage('Do it') {
+        stage('Do it 1') {
             withCredentials([sshUserPrivateKey(credentialsId: 'hepburn-pk', keyFileVariable: 'HEPBURN_PK')]) {
                 sh '''poetry run ansible -i inventory/hosts.yml roman -l hepburn.local -a 'free -h' --extra-vars "ansible_ssh_private_key_file=${HEPBURN_PK}" '''
+            }
+        }
+        stage('Sleep') {
+            sleep 60
+        }
+        stage('Do it 2') {
+            withCredentials([sshUserPrivateKey(credentialsId: 'peck-pk', keyFileVariable: 'PECK_PK')]) {
+                sh '''poetry run ansible -i inventory/hosts.yml roman -l peck.local -a 'free -h' --extra-vars "ansible_ssh_private_key_file=${PECK_PK}" '''
             }
         }
     }
