16ece4b5283ce90f50acd8b39f31b27c755c0776
Anthony Villena <hiddenwaffle@users.noreply.github.com>
2021-02-02 12:50:01 -0600

    next

--- pipelines/ansible.groovy
+++ pipelines/ansible.groovy
@@ -18,7 +18,7 @@ void doIt(command, fn) {
         stage('Setup') {
             checkout scm
         }
-        image = docker.build("tempdeleteme:latest")
+        image = docker.build('tempdeleteme:latest')
         image.inside('--add-host=hepburn.local:192.168.65.2 --add-host=peck.local:192.168.65.2') {
             withCredentials([
                 sshUserPrivateKey(credentialsId: 'hepburn-pk', keyFileVariable: 'HEPBURN_PK'),
